e90ed2f9cb36859954187dbdee9669f383c37ec7
Al Azad <alazad214@gmail.com>
2024-05-11 19:27:35 +0600

    app ui complete

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
 import 'home_screen.dart';
@@ -14,13 +15,7 @@ class _SplashState extends State<Splash> {
   @override
   void initState() {
     Future.delayed(const Duration(seconds: 5), () {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (
-            _,
-          ) =>
-                  const HomeScreen()));
+      Get.offAll(HomeScreen());
     });
     super.initState();
   }
