112c597318472a89c812a7eeec7b4ee284d97545
Al Azad <alazad214@gmail.com>
2024-05-12 20:10:21 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
-        title: 'Inspiration App',
+        title: 'Trip app',
         debugShowCheckedModeBanner: false,
         home: Splash());
   }
