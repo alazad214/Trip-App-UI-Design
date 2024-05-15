9d51f111fc34a5c03be1ec0bef98c78fee5638c3
Al Azad <alazad214@gmail.com>
2024-05-15 22:53:51 +0600

    trip ui start

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,53 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          children: [
+            makepage(),
+            Container(
+              decoration: const BoxDecoration(color: Colors.amberAccent),
+            ),
+            Container(
+              decoration: const BoxDecoration(color: Colors.deepOrange),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget makepage() {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                "assets/images/pic1.jpg",
+              ),
+              fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient:
+                LinearGradient(begin: Alignment.bottomCenter, stops: const [
+          0.1,
+          0.9
+        ], colors: [
+          Colors.black.withOpacity(0.5),
+          Colors.black.withOpacity(0.1),
+        ])),
+        child: const Column(
+          children: [
+            Text(
+              "1",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
